95759fe30e061035db5af81c66c2b6dcc0caa36a
Arnav Bagad <arnavb@cs.utexas.edu>
2024-04-17 16:47:43 -0500

    did some work

--- main.cxx
+++ main.cxx
@@ -99,111 +99,172 @@ class Interpreter {
 public:
     Interpreter(char const* prog): program(prog), current(prog) {}
 
-    // The plan is to honor as many C operators as possible with
-    // the same precedence and associativity
+    // The plan is to honor as many Verilog operators as possible
     // e<n> implements operators with precedence 'n' (smaller is higher)
 
-    // () [] . -> ...
+    // () [] i.e. getting bits from a wire, array indexing
     uint64_t e1(bool effects) {
-        if (auto id = consume_identifier()) {
+
+        //add here: consuming $ for $write, $readmem, $signed.
+
+        //TODO: Handle arrays as different identifiers
+        if (auto id = consume_identifier()) { 
             auto v = symbol_table[id.value()];
+
+
+            if (consume("[")) {
+            
+                auto v = expression(effects);
+
+                    if (consume(":")) {
+                        //get other end of bits to consider
+                        auto v2 = expression(effects);
+                    }
+
+                consume("]");
+                //TODO: either get the bit values or array values
+                //for bit value need [value : value]
+                //for array indexing just [value]
+            }
+
             return v;
         } else if (auto v = consume_literal()) {
-            return v.value();
+            return v.value(); //TODO: Make literals also take binary, hex, oct
         } else if (consume("(")) {
             auto v = expression(effects);
             consume(")");
             return v;
+            
         } else {
+            //TODO: need to change this to returning null for unary operators instead of fail.
+            //i.e. null!A, null~A => !A, ~A (just complement of A)
             fail();
         }
     }
 
-    // ++ -- unary+ unary- ... (Right)
+    // &A ~&A |A ~|A ^A ~^A (Reduction, not implementing)
+    // !A, ~A, +A, -A (Complement, Unary operators)
     uint64_t e2(bool effects) {
-        return e1(effects);
+        auto v = e1(effects);
+
+        //v must be null, otherwise return v.
+        if (v != NULL) return v; 
+        while (true) {
+            if (consume("!")) {
+                v = e1(effects) != 0 ? 0 : 1;
+            } else if (consume("~")) {
+                v = ~e1(effects);
+            } else if (consume("+")) {
+                auto right = e1(effects);
+                v = right < 0 ? -right : right;
+            } else if (consume("-")) {
+                auto right = e1(effects);
+                v = right > 0 ? -right : right;
+            } 
+            else {
+                return v;
+            }
+        }
+
     }
 
-    // * / % (Left)
+    // {N{A}}, {A, B, ..., C} => We need to move comma to e15, and make {} run all ex again.
     uint64_t e3(bool effects) {
         auto v = e2(effects);
+        //v is the number of times to repeat. 1 = don't repeat
+
+        
+        return v;
+    }
+
+    // (Left) * / %
+    uint64_t e4(bool effects) {
+        auto v = e3(effects);
 
         while (true) {
             if (consume("*")) {
-                v = v * e2(effects);
+                v = v * e3(effects);
             } else if (consume("/")) {
-                auto right = e2(effects);
+                auto right = e3(effects);
                 v = (right == 0) ? 0 : v / right;
             } else if (consume("%")) {
-                auto right = e2(effects);
+                auto right = e3(effects);
                 v = (right == 0) ? 0 : v % right;
             } else {
                 return v;
             }
         }
+
+
     }
 
     // (Left) + -
-    uint64_t e4(bool effects) {
-        auto v = e3(effects);
-
+    uint64_t e5(bool effects) {
+        auto v = e4(effects);
         while (true) {
             if (consume("+")) {
-                v = v + e3(effects);
+                v = v + e4(effects);
             } else if (consume("-")) {
-                v = v - e3(effects);
+                auto right = e4(effects);
+                v = v - e4(effects);
             } else {
                 return v;
             }
         }
     }
 
-    // << >>
-    uint64_t e5(bool effects) {
-        return e4(effects);
-    }
-
-    // < <= > >=
+    // <<, >>
     uint64_t e6(bool effects) {
-        return e5(effects);
+        auto v = e5(effects);
+        while (true) {
+            if (consume("<<")) {
+                v = v << e5(effects);
+            } else if (consume(">>")) {
+                v = v >> e5(effects);
+            } else {
+                return v;
+            }
+        }
     }
 
-    // == !=
+    // A>B A<B A>=B A<=B
     uint64_t e7(bool effects) {
         return e6(effects);
     }
 
-    // (left) &
+    // A==B A!=B
     uint64_t e8(bool effects) {
         return e7(effects);
     }
 
-    // ^
+    // A&B
+
     uint64_t e9(bool effects) {
         return e8(effects);
     }
 
-    // |
+    // A^B A~^B
+
     uint64_t e10(bool effects) {
         return e9(effects);
     }
 
-    // &&
+    // A|B
     uint64_t e11(bool effects) {
         return e10(effects);
     }
 
-    // ||
+    // A&&B
     uint64_t e12(bool effects) {
         return e11(effects);
     }
 
-    // (right with special treatment for middle expression) ?:
+    // A||B
     uint64_t e13(bool effects) {
         return e12(effects);
     }
 
-    // = += -= ...
+    // ?:
     uint64_t e14(bool effects) {
         return e13(effects);
     }
@@ -218,13 +279,58 @@ public:
     }
 
     bool statement(bool effects) {
-        if (consume("print")) {
-            // print ...
+        if (consume("wire")) {
+            // wire varName;
+            // wire varName = expression;
+            // wire [x:y] varName;
+            // wire [x:y] varName = expression;
+            
+            // TODO: Implement this
+
+            /*
+            auto id = consume_identifier();
             auto v = expression(effects);
             if (effects) {
                 printf("%ld\n",v);
             }
             return true;
+            */
+        } else if (consume("reg")) {
+            // reg varName;
+            // reg varName = expression;
+            // reg [x:y] varName;
+            // reg [x:y] varName = expression;
+            // reg [x:y] varName [a:b];
+            // reg [x:y] varName [a:b] = expression;
+
+            // TODO: Implement this
+
+            /*
+            auto id = consume_identifier();
+            auto v = expression(effects);
+            if (effects) {
+                printf("%ld\n",v);
+            }
+            return true;
+            */
+        } else if (consume("module")) {
+            // module varName(params);
+            // reg varName = expression;
+            // reg [x:y] varName;
+            // reg [x:y] varName = expression;
+            // reg [x:y] varName [a:b];
+            // reg [x:y] varName [a:b] = expression;
+
+            // TODO: Implement this
+
+            /*
+            auto id = consume_identifier();
+            auto v = expression(effects);
+            if (effects) {
+                printf("%ld\n",v);
+            }
+            return true;
+            */
         } else if (auto id = consume_identifier()) {
             // x = ...
             if (consume("=")) {
--- value.h
+++ value.h
@@ -4,15 +4,13 @@
 #include <string>
 #include <stdint.h>
 
-class Value{
-    public:
-        uint64_t val;
-        uint64_t ndef;
+class Value {
+public:
+    uint64_t val;
+    uint64_t ndef;
 
+    // Constructor
+    Value(uint64_t value, uint64_t undefined) : val(value), ndef(undefined) {}
 
-        std::string toString();
-        
-        
-
-
-}
+    std::string toString();
+};
